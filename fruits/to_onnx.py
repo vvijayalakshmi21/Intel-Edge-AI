ea52ce0ae49c716e33ffa2b1f884713c03a0b838
LakshyaMalhotra <lakshya9009@gmail.com>
2020-02-20 08:02:08 -0500

    added model and onnx files

---
+++ fruits/to_onnx.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+import torch.onnx as onnx
+import torchvision.models as models
+from collections import OrderedDict
+
+# Using CPU
+device = torch.device('cpu')
+
+# loading the pretrained densenet 169 model
+model = models.densenet169(pretrained=True)
+
+# changing the classifier according to our model, 120 is the total number of classes
+classifier = nn.Sequential(OrderedDict([
+    ('fc1', nn.Linear(1664, 500)),
+    ('relu', nn.ReLU()),
+    ('fc2', nn.Linear(500, 120))
+]))
+
+model.classifier = classifier
+
+# loading the model's state dictionary
+model.load_state_dict(torch.load('fruits.pt', map_location=device))  # converting the model from GPU to CPU
+
+# dummy variable
+x = torch.randn(1, 3, 100, 100)         # size of the input image
+
+# exporting to ONNX
+onnx.export(model, x, "fruits.onnx", verbose=True, export_params=True)
